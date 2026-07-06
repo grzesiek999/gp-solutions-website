2e677d1b18da960b51e81ebfb7fef19eea477b2d
Grzesiek Pasich <grzegorz.pasich@o2.pl>
2026-07-06 15:47:36 +0200

    home page bottom chunk complete

---
+++ client/src/components/HomePageContent/HomePageContent.tsx
@@ -0,0 +1,17 @@
+import styles from "./HomePageContent.module.scss"
+
+const HomePageContent = () => {
+    return (
+        <div className={styles.homePageContentMainDiv}>
+            <div>
+                <span>Dlaczego warto nam zaufać?</span>
+            </div>
+            <div>
+                <span>Nasze usługi</span>
+
+            </div>
+        </div>
+    )
+}
+
+export default HomePageContent
--- client/src/pages/HomePage/HomePage.tsx
+++ client/src/pages/HomePage/HomePage.tsx
@@ -1,11 +1,15 @@
 import HomePageBanner from "../../components/HomePageBanner/HomePageBanner.tsx";
+import HomePageContent from "../../components/HomePageContent/HomePageContent.tsx";
+import HomePageLastChunk from "../../components/HomePageLastChunk/HomePageLastChunk.tsx";
 
 
 const HomePage = () => {
     return (
-        <div>
+        <main>
             <HomePageBanner />
-        </div>
+            <HomePageContent />
+            <HomePageLastChunk />
+        </main>
     )
 }
 
