42258e69a7455499b207d6e424c3d0324c359a3f
Grzesiek Pasich <grzegorz.pasich@o2.pl>
2026-07-21 13:47:58 +0200

    add scroll to top when change url

--- client/src/layouts/WebsiteLayout/WebsiteLayout.tsx
+++ client/src/layouts/WebsiteLayout/WebsiteLayout.tsx
@@ -1,10 +1,12 @@
 import {Outlet} from "react-router-dom";
 import Header from "../../components/Header/Header.tsx";
 import Footer from "../../components/Footer/Footer.tsx";
+import ScrollToTop from "../../hooks/ScrollToTop.tsx";
 
 const WebsiteLayout = () => {
     return (
         <>
+            <ScrollToTop />
             <Header />
             <Outlet/>
             <Footer/>
