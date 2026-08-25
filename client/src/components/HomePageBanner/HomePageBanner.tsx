7305b9d865a6ce0a4ebc3b9abf5c9d284a8f40c2
Grzesiek Pasich <grzegorz.pasich@o2.pl>
2026-08-25 23:53:55 +0200

    responsive hamepage in proggers, contents div to reapis to rows

--- client/src/components/HomePageBanner/HomePageBanner.tsx
+++ client/src/components/HomePageBanner/HomePageBanner.tsx
@@ -1,23 +1,37 @@
 import styles from "./HomePageBanner.module.scss"
 import Button from "../Button/Button.tsx";
 import {useNavigate} from "react-router-dom";
+import {useMedia} from "use-media";
 
 const HomePageBanner = () => {
+
     const navigate = useNavigate();
+    const isMobile = useMedia({ maxWidth: 768 });
+
 
     return (
         <div className={styles.homePageBannerDiv}>
             <div className={styles.hamePageBannerContent}>
                 <span>Kompleksowa opieka IT dla firm</span>
-                <p>Bezpieczeństwo. Niezawodność. Wsparcie. Skup się na swoim biznesie - <br/> my zajmiemy się IT.</p>
+                {isMobile ?
+                    <>
+                        <p>Bezpieczeństwo. Niezawodność. Wsparcie. </p>
+                        <p>Skup się na swoim biznesie - <br/> my zajmiemy się IT.</p>
+                    </>:
+                    <p>Bezpieczeństwo. Niezawodność. Wsparcie. Skup się na swoim biznesie - <br/> my zajmiemy się IT.</p>
+                }
                 <div className={styles.homePageBannerButtonsDiv}>
                     <Button
                         className={styles.homePageBannerContactButton}
                         type={"button"}
                         onClick={()=> navigate('/contact')}
                     >
-                        <div>
-                            <img src={'src/assets/icons/white-phone-icon.png'} alt='white phone icon error' />
+                        <div className={styles.homePageBannerButtonDiv}>
+                            <img
+                                src={'src/assets/icons/white-phone-icon.png'}
+                                alt='white phone icon error'
+                                className={styles.homePageBannerButtonImg}
+                            />
                             <span className={styles.homePageBannerButtonsSpan}>Skontaktuj się</span>
                         </div>
                     </Button>
@@ -26,8 +40,12 @@ const HomePageBanner = () => {
                         type={"button"}
                         onClick={()=> navigate('/services')}
                     >
-                        <div>
-                            <img src={'src/assets/icons/white-mail-icon.png'} alt='white mail icon error' />
+                        <div className={styles.homePageBannerButtonDiv}>
+                            <img
+                                src={'src/assets/icons/white-mail-icon.png'}
+                                alt='white mail icon error'
+                                className={styles.homePageBannerButtonImg}
+                            />
                             <span className={styles.homePageBannerButtonsSpan}>Sprawdź usługi</span>
                         </div>
                     </Button>
