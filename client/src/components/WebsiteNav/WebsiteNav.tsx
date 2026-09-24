cdbcab9ae29fbfd5fece4fea9a79bb4c89812068
Grzesiek Pasich <grzegorz.pasich@o2.pl>
2026-09-24 20:08:16 +0200

    srvices menu in proggres

--- client/src/components/WebsiteNav/WebsiteNav.tsx
+++ client/src/components/WebsiteNav/WebsiteNav.tsx
@@ -8,9 +8,36 @@ const NAV_ITEMS = [
     { label: "Kontakt", path: "/contact" }
 ];
 
+const SERVICES_NAV_ITEMS = [
+    { label: "Strony internetowe", path: "/services/websites" },
+    { label: "Dedykowane systemy", path: "/services/systems" },
+    { label: "Automatyzacja", path: "/services/automation" },
+    { label: "Integracje systemów", path: "/services/integration" },
+    { label: "Opieka IT", path: "/services/itcare" },
+    { label: "Cyberbezpieczeństwo", path: "/services/cybersecurity" },
+    { label: "Ochrona danych", path: "/services/backups" },
+    { label: "Szkolenia pracowników", path: "/services/education" },
+]
+
 type WebsiteNavHeaderProps = {
     setOpen?: React.Dispatch<React.SetStateAction<boolean>>;
 }
+
+const ServicesList = (
+    <div className={styles.servicesListDiv}>
+        <ul>
+            {SERVICES_NAV_ITEMS.map((item) => (
+                <li key={item.path}>
+                    <NavLink
+                        to={item.path}
+                        end={item.path === '/'}
+                        className={({isActive}) => isActive ? styles.active : ""}
+                    >{item.label}</NavLink>
+                </li>
+            ))}
+        </ul>
+    </div>
+)
 
 const WebsiteNavHeader = ({ setOpen }: WebsiteNavHeaderProps) => {
 
@@ -26,6 +53,7 @@ const WebsiteNavHeader = ({ setOpen }: WebsiteNavHeaderProps) => {
                                 onClick={()=>{if(setOpen) setOpen(false);}}
                                 className={({isActive}) => isActive ? styles.active : ""}
                             >{item.label}</NavLink>
+                            {item.path === '/services' && ServicesList}
                         </li>
                     ))}
                 </ul>
